067de4c30fdeeb2a1a56d4f346eebf4c4ab5824a
IAmHeroForFun <everythinga1360@gmail.com>
2026-08-27 20:00:49 +0530

    fix: use same directory for req.txt

--- build_exe.bat
+++ build_exe.bat
@@ -1,4 +1,5 @@
 @echo off
+cd /d "%~dp0"
 echo ===================================================
 echo FastPST - Windows Executable (.exe) Builder
 echo ===================================================
